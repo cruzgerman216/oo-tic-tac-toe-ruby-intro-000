807c9a9c18f021832df3f4e4c308c35f159c4c6a
German Cruz <cruzgerman216@yahoo.com>
2020-02-23 01:01:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,18 +1,18 @@
 class TicTacToe
   def initialize(board = nil)
   @board = board || Array.new(9, " ")
-  @WIN_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [6, 4, 2]
-  ].freeze
-end
 
+end
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+].freeze
 def current_player
   turn_count % 2 == 0 ? "X" : "O"
 end
