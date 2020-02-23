66c3b1e85e0586921af258c0c9cc6e05e1435233
German Cruz <cruzgerman216@yahoo.com>
2020-02-23 00:57:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,8 +29,61 @@ def display_board
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
 
+def valid_move?(index)
+  index.between?(0, 8) && !position_taken?(@board, index)
+end
+
+def won?
+  WIN_COMBINATIONS.detect do |combo|
+    @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[1]] == @board[combo[2]] &&
+      position_taken?(@board, combo[0])
+  end
+end
+
+def full?
+  @board.all? { |token| token == 'X' || token == 'O' }
+end
+
+def draw?
+  !won?(@board) && full?(@board)
+end
+
+def over?
+  won?(@board) || draw?(@board)
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def turn
+  puts 'Please enter 1-9:'
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?( index)
+    move(board, index, current_player(board))
+    display_board
+  else
+    turn
+  end
+end
+
+def position_taken?(index)
+  @board[index] == 'X' || @board[index] == 'O'
+end
+
+def move (index, player)
+  @board[index] = player
+end
+
+def winner
+  if winning_combo = won?(board)
+    @board[winning_combo.first]
+  end
+end
 
   def play
-    puts @WIN_COMBINATIONS[0]
+    
   end
 end
