090c4ed487e408c56bdbe92f600cc28435a53b45
German Cruz <cruzgerman216@yahoo.com>
2020-02-23 00:44:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,6 +19,18 @@ def display_board
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
 
+def WIN_COMBINATIONS 
+@WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+].freeze
+end
   def play
     puts display_board
   end
